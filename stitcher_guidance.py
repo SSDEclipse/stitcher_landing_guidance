0fd45709cf12a4f3adf481423139da8dfb6c28d4
SSDEclipse <krithi.koodli@gmail.com>
2026-06-04 23:58:02 -0700

    backing up

--- stitcher_guidance.py
+++ stitcher_guidance.py
@@ -12,15 +12,16 @@ class Vehicle:
         self.max_thrust = max_thrust
         self.min_thrust = min_thrust
         self.Isp = Isp
+        self.v_e = Isp * 9.80665
 
 
 class Node:
-    def __init__(self, position_array, velocity_array, time, mass):
+    def __init__(self, position_array, velocity_array, time):
         self.position = position_array
         self.velocity = velocity_array
         self.time = time
-        self.mass = mass
-        self.edges = []
+        self.target_edges = []
+        self.parent_edges = []
     
     # def create_and_append_edge(self, target_node):
 
@@ -32,51 +33,85 @@ class Node:
     #     self.edges.append(new_edge)
     #     return new_edge
     
-    def create_and_append_constant_accel_edge(self, target_node):
-        t_f = target_node.arrival_time
-        c_0_array = 2.0 / t_f**2 * (target_node.position - self.position - self.velocity * t_f)
-        c_0_array[0] += planetary_body_config.body_surface_gravity
-        new_edge = Edge(target_node, c_0_array, np.array([0.0, 0.0, 0.0]), t_f)
-        self.edges.append(new_edge)
-        return new_edge
+    def create_and_append_constant_accel_edge(self, target_node, mode):
+        if mode == 'forward':
+            t_f = target_node.time
+            if t_f == 0:
+                c_0_array = np.array([0.0, 0.0, 0.0])
+                v_f = self.velocity
+                target_node.update_node_velocity(v_f)
+            else:
+                c_0_array = 2.0 / t_f**2 * (target_node.position - self.position - self.velocity * t_f)
+                v_f = self.velocity + c_0_array*t_f
+                c_0_array[0] += planetary_body_config.body_surface_gravity
+                target_node.update_node_velocity(v_f)
+            new_edge = Edge(self, target_node, c_0_array, np.array([0.0, 0.0, 0.0]), t_f)
+            self.target_edges.append(new_edge)
+            target_node.parent_edges.append(new_edge)
+            return new_edge
+        
+        elif mode == 'backward':
+            t_f = target_node.time
+            t_f = target_node.time
+            if t_f == 0:
+                c_0_array = np.array([0.0, 0.0, 0.0])
+                v_0 = target_node.velocity
+                self.update_node_velocity(v_0)
+            else:
+                c_0_array = -2.0 / t_f**2 * (target_node.position - self.position - target_node.velocity * t_f)
+                v_0 = target_node.velocity - c_0_array*t_f
+                c_0_array[0] += planetary_body_config.body_surface_gravity
+                self.update_node_velocity(v_0)
+            new_edge = Edge(self, target_node, c_0_array, np.array([0.0, 0.0, 0.0]), t_f)
+            self.target_edges.append(new_edge)
+            target_node.parent_edges.append(new_edge)
+            return new_edge
     
     def update_node_velocity(self, input_velocity):
         self.velocity = input_velocity
 
-    def update_node_mass(self, input_mass):
-        self.mass = input_mass
 
 
 class Edge:
-    def __init__(self, target_node, c0_array, c1_array, t_f):
+    def __init__(self, parent_node, target_node, c0_array, c1_array, t_f):
+        self.parent_node = parent_node
         self.target_node = target_node
         self.c0_array = c0_array
         self.c1_array = c1_array
         self.t_f = t_f
+        self.mass_consumed = None
+        self.end_mass = None
 
-    def cost(self):
-        accel_squared_integral = 0
-        for i in range(len(self.c0_array)):
-            accel_squared_integral += self.c0_array[i]**2 * self.t_f + self.c0_array[i]*self.c1_array[i] * self.t_f**2 + 1.0/3.0*self.c1_array[i]**2 * self.t_f**3
-        # TODO: fix with thrust integral instead
-        return accel_squared_integral
 
-    
-    def check_thrust_bounds(self, mass, thrust_bound_lower, thrust_bound_upper):
-        t_array = np.linspace(0, self.t_f, 10)
-        for time in t_array:
-            accel_mag = self.c0_array + self.c1_array * time
-            thrust_mag = accel_mag * mass
-            if thrust_mag > thrust_bound_upper or thrust_mag < thrust_bound_lower:
-                return False
+    def compute_const_accel_mass_consumed(self, edge_start_mass, v_e):
+
+        thrust_integral = edge_start_mass * v_e * (1 - np.exp(-np.linalg.norm(self.c0_array)* self.t_f / v_e))
+        
+        self.mass_consumed = thrust_integral / v_e
+
+        self.end_mass = edge_start_mass - self.mass_consumed
+
+    def check_const_accel_thrust_bounds(self, edge_start_mass, thrust_bound_lower, thrust_bound_upper):
+        accel_mag = np.linalg.norm(self.c0_array)
+        # only need to check thrust at start of edge since that's when it would
+        # be highest for constant accel
+        max_thrust_mag = accel_mag * edge_start_mass
+        if max_thrust_mag > thrust_bound_upper or max_thrust_mag < thrust_bound_lower:
+            return False
         return True
 
+    # def check_linear_accel_thrust_bounds(self, thrust_bound_lower, thrust_bound_upper):
+    #     t_array = np.linspace(0, self.t_f, 10)
+    #     for time in t_array:
+    #         accel_mag = np.linalg.norm(self.c0_array + self.c1_array * time)
+    #         thrust_mag = accel_mag * mass
+    #         if thrust_mag > thrust_bound_upper or thrust_mag < thrust_bound_lower:
+    #             return False
+    #     return True
 
-def generate_discrete_set(lower_bound, upper_bound, num_points):
-    return np.linspace(lower_bound, upper_bound, num_points)
 
-def generate_time_set(discrete_time_array):
-    return discrete_time_array
+
+
 
 def generate_position_set(pos_x, pos_y, pos_z):
     output = []
@@ -87,54 +122,140 @@ def generate_position_set(pos_x, pos_y, pos_z):
     output = np.asarray(output)
     return output
 
-def generate_velocity_set(vel_zenith, vel_azimuth, vel_mag):
-    output = []
-    for i in range(len(vel_zenith)):
-        for j in range(len(vel_azimuth)):
-            for k in range(len(vel_mag)):
-                vel_x = vel_mag[k] * np.sin(vel_zenith[i]) * np.cos(vel_azimuth[j])
-                vel_y = vel_mag[k] * np.sin(vel_zenith[i]) * np.sin(vel_azimuth[j])
-                vel_z = vel_mag[k] * np.cos(vel_zenith[i])
-                output.append(np.array([vel_x, vel_y, vel_z]))
-    output = np.asarray(output)
-    return output
 
 
 
 def generate_phase_1_nodes(initial_r):
 
-    sampled_time_array = generate_discrete_set(0.1, 10, 5)
-    sampled_position_x_array = generate_discrete_set(0, initial_r[0], 5)
-    sampled_position_y_array = generate_discrete_set(0, initial_r[1], 5)
-    sampled_position_z_array = generate_discrete_set(0, initial_r[2], 5)
+    sampled_time_array = np.linspace(0.0, 10, 5)
+    sampled_position_x_array = np.linspace(0, initial_r[0], 5)
+    sampled_position_y_array = np.linspace(0, initial_r[1], 5)
+    sampled_position_z_array = np.linspace(0, initial_r[2], 5)
     sampled_positions_array = generate_position_set(sampled_position_x_array, sampled_position_y_array, sampled_position_z_array)
 
     phase_1_nodes = []
     for time in sampled_time_array:
         for pos in sampled_positions_array:
-            phase_1_nodes.append(Node(pos, None, time, None))
+            phase_1_nodes.append(Node(pos, None, time))
+    return phase_1_nodes
+
+def generate_phase_2_nodes(initial_r):
+
+    sampled_time_array = np.linspace(0.1, 10, 5)
+    sampled_position_x_array = np.linspace(0, initial_r[0], 5)
+    sampled_position_y_array = np.linspace(0, initial_r[1], 5)
+    sampled_position_z_array = np.linspace(0, initial_r[2], 5)
+    sampled_positions_array = generate_position_set(sampled_position_x_array, sampled_position_y_array, sampled_position_z_array)
+
+    phase_2_nodes = []
+    for time in sampled_time_array:
+        for pos in sampled_positions_array:
+            phase_2_nodes.append(Node(pos, None, time))
+    return phase_2_nodes
+
+def generate_phase_3_nodes(target_r, target_v):
+
+    sampled_time_array = np.linspace(0.1, 10, 5)
+
+    phase_3_nodes = []
+    for time in sampled_time_array:
+        phase_3_nodes.append(Node(target_r, target_v, time))
+    return phase_3_nodes
 
 
-def generate_stitcher_trajectory_constant_accel(vehicle, initial_r, initial_v, target_r, target_v):
+def prune_edges(input_mass_array, thrust_lower_bound, thrust_upper_bound, input_edges, input_nodes=None):
 
-    start_node = Node(initial_r, initial_v, 0.0, vehicle.wet_mass)
+    # for edge in input_edges[:]:
+    #     prune_edge_flag = False
 
-    # phase 1
+    #     # check all constraints here
+    #     if not edge.check_const_accel_thrust_bounds(input_mass, thrust_lower_bound, thrust_upper_bound):
+    #         prune_edge_flag = True
+    #     # elif ...
 
-    generate_phase_1_nodes(initial_r)
+    #     if prune_edge_flag:
+    #         input_edges.remove(edge)
+    #         if not input_nodes == None:
+    #             input_nodes.remove(edge.target_node)
+    input_edges_copy = input_edges[:]
+    for edg_idx in range(len(input_edges_copy)):
+        edge = input_edges_copy[edg_idx]
+        prune_edge_flag = False
+
+        # check all constraints here
+        if not edge.check_const_accel_thrust_bounds(input_mass_array[edg_idx], thrust_lower_bound, thrust_upper_bound):
+            prune_edge_flag = True
+        # elif ...
+
+        if prune_edge_flag:
+            input_edges.remove(edge)
+            if not input_nodes == None:
+                input_nodes.remove(edge.target_node)
+
+def compute_edge_costs(v_e, input_edges):
+    for edge in input_edges:
+        edge.compute_const_accel_mass_consumed(v_e)
 
 
-    def generate_phase_1_edges_nodes(time_array, position_set, velocity_set):
 
-        phase_2_nodes = []
 
-        for time in time_array:
-            for position in position_set:
-                for velocity in velocity_set:
-                    phase_2_nodes.append(Node(position, velocity, time))
-        for node in phase_2_nodes:
-            start_node.create_and_append_edge(node)
-        return phase_2_nodes
+
+def generate_stitcher_trajectory_constant_accel(vehicle, initial_r, initial_v, final_r, final_v):
+
+    start_node = Node(initial_r, initial_v, 0.0)
+
+    # create phase 1 nodes
+
+    phase_1_nodes = generate_phase_1_nodes(initial_r)
+
+    # connect start and phase 1 nodes with edges
+    for phase_1_node in phase_1_nodes:
+        start_node.create_and_append_constant_accel_edge(phase_1_node, mode='forward')
+
+    # compute costs of phase 1 edges
+    for start_edge in start_node.target_edges:
+        start_edge.compute_const_accel_mass_consumed(vehicle.wet_mass, vehicle.v_e)
+
+    # prune phase 1 edges and nodes that violate constraints
+    prune_edges([vehicle.wet_mass]*len(start_node.target_edges), vehicle.min_thrust, vehicle.max_thrust, start_node.target_edges, phase_1_nodes)
+    
+    # create phase 3 nodes
+    phase_3_nodes = generate_phase_3_nodes(final_r, final_v)
+
+    # create phase 2 nodes
+    phase_2_nodes = generate_phase_2_nodes(initial_r)
+
+    # connect phase 3 and phase 2 nodes with edges
+    for phase_2_node in phase_2_nodes:
+        for phase_3_node in phase_3_nodes:
+            phase_2_node.create_and_append_constant_accel_edge(phase_3_node, mode='backward')
+
+    # connect phase 1 and phase 2 nodes with edges
+    for phase_1_node in phase_1_nodes:
+        for phase_2_node in phase_2_nodes:
+            phase_1_node.create_and_append_constant_accel_edge(phase_2_node, mode='forward')
+
+
+
+
+
+    # compute costs of phase 2 edges
+    counter = 0
+    for phase_1_node in phase_1_nodes:
+        for edg_idx in range(len(phase_1_node.target_edges)):
+            phase_1_node.target_edges[edg_idx].compute_const_accel_mass_consumed(phase_1_node.parent_edges[0].end_mass, vehicle.v_e)
+            counter += 1
+    print(counter)
+
+    # compute costs of phase 3 edges
+    counter = 0
+    for phase_2_node in phase_2_nodes:
+        for edg_idx in range(len(phase_2_node.target_edges)):
+            phase_2_node.target_edges[edg_idx].compute_const_accel_mass_consumed(phase_2_node.parent_edges[edg_idx].end_mass, vehicle.v_e)
+            counter += 1
+    print(counter)
+
+    
 
 
 
